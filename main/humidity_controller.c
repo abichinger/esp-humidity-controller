6a6f000c18cb998779a4e220a8b2aad5f3e0332e
Andreas Bichinger <andreas.bichinger@gmail.com>
2021-02-26 15:04:23 +0100

    add wifi ap

--- main/humidity_controller.c
+++ main/humidity_controller.c
@@ -7,19 +7,37 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
 
 #include "DHT22.h"
 
+
 #define HUMIDITY_THRESHOLD 60.0
 
+#define ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
+#define MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+
+#define DHT22_GPIO CONFIG_DHT22_GPIO
+#define RELAY_GPIO CONFIG_RELAY_GPIO
+
+esp_netif_t *esp_netif;
+
+static const char *TAG = "HC";
+
 void DHT_task(void *pvParameter){
-	setDHTgpio( GPIO_NUM_23 );
+	setDHTgpio( DHT22_GPIO );
 	printf( "Starting DHT Task\n\n");
 
 	while(1) {
@@ -39,9 +57,9 @@ void DHT_task(void *pvParameter){
 }
 
 void Relay_task(void *pvParameter){
-   gpio_pad_select_gpio( GPIO_NUM_22 );
+   gpio_pad_select_gpio( RELAY_GPIO );
     /* Set the GPIO as a push/pull output */
-   gpio_set_direction(GPIO_NUM_22, GPIO_MODE_OUTPUT);
+   gpio_set_direction(RELAY_GPIO, GPIO_MODE_OUTPUT);
 
    float humidity = 0.0;
    bool relay_high = false;
@@ -52,20 +70,82 @@ void Relay_task(void *pvParameter){
 
       if(humidity > HUMIDITY_THRESHOLD+1 && !relay_high){
          printf("Turning on the relay\n");
-         gpio_set_level(GPIO_NUM_22, 1);
+         gpio_set_level(RELAY_GPIO, 1);
          relay_high = true;
       }
       else if(humidity < HUMIDITY_THRESHOLD-1 && relay_high){
          printf("Turning off the relay\n");
-         gpio_set_level(GPIO_NUM_22, 0);
+         gpio_set_level(RELAY_GPIO, 0);
          relay_high = false;
       }
       vTaskDelay(1000 / portTICK_PERIOD_MS);
    }
 }
 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data){
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d", MAC2STR(event->mac), event->aid);
+    } 
+    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_ap(void){
+   esp_netif = esp_netif_create_default_wifi_ap();
+
+   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+   ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = ESP_WIFI_SSID,
+            .ssid_len = strlen(ESP_WIFI_SSID),
+            .channel = ESP_WIFI_CHANNEL,
+            .password = ESP_WIFI_PASS,
+            .max_connection = MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    if (strlen(ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             ESP_WIFI_SSID, ESP_WIFI_PASS, ESP_WIFI_CHANNEL);
+}
+
+void wifi_init_sta(void){
+
+}
+
 void app_main(void){
-   nvs_flash_init();
+
+   //init nvs
+   esp_err_t ret = nvs_flash_init();
+   if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+   }
+   ESP_ERROR_CHECK(ret);
+
+   //init tcp/ip stack
+   ESP_ERROR_CHECK(esp_netif_init());
+
+   //init default event loop
+   ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+   wifi_init_ap();
+
+   //init tasks
 	vTaskDelay( 1000 / portTICK_RATE_MS );
 	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
    xTaskCreate( &Relay_task, "Relay_task", 2048, NULL, 5, NULL );
